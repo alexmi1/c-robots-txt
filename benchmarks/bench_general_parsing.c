5d707a0b60553e98e8fb637d34df9bb2aa19ae19
Alex Miranda <37449977+alexmi1@users.noreply.github.com>
2025-05-13 19:55:41 -0300

    Edit a comment

--- benchmarks/bench_general_parsing.c
+++ benchmarks/bench_general_parsing.c
@@ -4,11 +4,14 @@
 #include <stdbool.h>
 
 int main(void) {
-
     // atm on my machine this takes 1.3 seconds, while a popular implementation in C++ takes
     // 5.478 seconds, YMMV of course. also this is not a realistic robots.txt file, but idk if
-    // adding random websites' robots.txt files to this repo is allowed. if you have realistic,
-    // big robots.txt files to contribute, please open a PR
+    // adding random websites' robots.txt files to this repo is allowed
+
+    // I tried a rust port version of said library and it took 22.5 seconds while compiling
+    // much slower, having bloated binaries etc etc. yes it was a release build
+
+    // for proper benchmarking, could probably grab robots.txt files from CommonCrawl
 
     char* file_data = Utils_read_robots_file("../tests/test-files/general-parsing.txt");
     const int ITERATIONS = 1000000;
